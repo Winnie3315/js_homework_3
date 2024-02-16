a50cb1d61930b1e2a132f34d3d0c669069220ccc
Winnie3315 <winniiemae777@gmail.com>
2024-02-16 15:51:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let name = "Alex"
+ let code = 7777
+ let money = 10000
+ 
+ let user_name = prompt("как вас зовут?")
+ if ( name.toLowerCase().includes(user_name)) {
+   alert("Welcome")
+   let user_code = +prompt("Номер счета")
+   if (code === user_code) {
+     alert("успешно")
+   
+     let cash_out = +prompt("сколько обналичить?")
+     if (cash_out <= money) {
+       alert("Успешно")
+    
+
+       let balance = money - cash_out
+
+       if (balance <= 10000) {
+          alert( `Вы успешно сняли ' ${cash_out}.` )
+          alert(`Остаток на счете:'${balance}`) }
+    }
+
+
+   }else{alert("Пользователь не найден, досвидудули")}
+
+
+ } else {
+   alert("Пользователь не найден, досвидудули")
+ }
+ 
+  
+ 
+ 
+ 
+ 
+
